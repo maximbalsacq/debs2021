48442a7b5b2448496363546c6c8e02904d5ab84f
Maxim Balsacq <maxim.balsacq@hs-furtwangen.de>
2021-03-24 11:50:07 +0100

    Allow matching a point to a location
    Based on previous code in bin/parse_location.rs.
    Still untested!

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,67 @@
 pub mod gen;
+
+use crate::gen::challenger::Locations;
+use geo::{MultiPolygon,Rect,point,prelude::{Contains,BoundingRect}};
+
+#[derive(Debug)]
+pub struct AnalysisLocation {
+    zipcode: String,
+    city: String,
+    qkm: f64,
+    population: i32
+}
+
+impl From<crate::gen::challenger::Location> for AnalysisLocation {
+    fn from(loc: crate::gen::challenger::Location) -> Self {
+        Self {
+            zipcode: loc.zipcode,
+            city: loc.city,
+            qkm: loc.qkm,
+            population: loc.population,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AnalysisLocations {
+    locations: Vec<(AnalysisLocation, Rect<f64>, MultiPolygon<f64>)>
+}
+
+impl AnalysisLocations {
+    pub fn new(locations: Locations) -> Self {
+        use std::iter::FromIterator;
+        let locations = locations.locations.into_iter()
+            .map(|location| {
+                let multipoly = geo::MultiPolygon::from_iter(
+                    location
+                        .polygons
+                        .iter()
+                        .map(|poly| geo::Polygon::new(
+                            // exterior ring
+                            geo::LineString::from_iter(
+                                poly.points
+                                .iter()
+                                .map(|point| (point.latitude, point.longitude))),
+                            // interior ring (unused)
+                            vec![]
+                        ))
+                );
+                let boundingrect = multipoly.bounding_rect().expect("Location should contain >= 1 polygon");
+                (location.into(), boundingrect, multipoly)
+            })
+            .collect();
+        Self {
+            locations
+        }
+    }
+
+    pub fn localize(&self, latitude: f32, longitude: f32) -> impl Iterator<Item=&AnalysisLocation> {
+        self.locations
+            .iter()
+            .filter(move |(_, bounding_rect, bounding_poly)| {
+                let p = point!(x: f64::from(longitude), y: f64::from(latitude));
+                bounding_rect.contains(&p) && bounding_poly.contains(&p)
+            })
+            .map(|(location, _, _)| location)
+    }
+}
