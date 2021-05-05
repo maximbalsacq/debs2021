496d84cd96b33d2655f745beee0fa9d029f4e276
Maxim Balsacq <maxim.balsacq@hs-furtwangen.de>
2021-05-05 11:45:10 +0200

    Modify batch2sql to convert 10% of batches to SQL
    Previously, only 1 batch (batch 0) was converted.
    The different batches are converted in parallel
    using the tokio runtime, taking about 1m30 to convert.
    To prevent running out of file handles,
    a lock limiting the program to 400 batches at
    once is used.

--- src/bin/batch2sql.rs
+++ src/bin/batch2sql.rs
@@ -2,10 +2,7 @@ use debs2021::io::load_batch;
 use tokio::fs::File;
 use tokio::io::{AsyncWriteExt,BufWriter};
 
-#[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let root = std::env::var("DEBS_DATA_ROOT").expect("DEBS_DATA_ROOT not set!");
-    let batchnum = 0;
+async fn batch2sql(root: &str, batchnum: usize) -> Result<(), Box<dyn std::error::Error>> {
     let mut out = BufWriter::new(File::create(format!("{}/sql/{}/{}.sql", &root, batchnum / 1000, batchnum % 1000)).await?);
     let batch = load_batch(&root, batchnum).await?;
     out.write_all("BEGIN;\n".as_bytes()).await?;
@@ -36,4 +33,23 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     out.write_all("COMMIT;\n".as_bytes()).await?;
     out.flush().await?;
     Ok(())
+}
+
+async fn guarded_batch2sql(root: &str, batchnum: usize, file_limiting_sem: std::sync::Arc<tokio::sync::Semaphore>) -> Result<(), Box<dyn std::error::Error>> {
+    let lock = file_limiting_sem.acquire().await.expect("Failed to acquire file semaphore");
+    let res = batch2sql(&root, batchnum).await;
+    drop(lock);
+    res
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use std::sync::Arc;
+    let root = std::env::var("DEBS_DATA_ROOT").expect("DEBS_DATA_ROOT not set!");
+    let file_limiting_sem = Arc::new(tokio::sync::Semaphore::new(400));
+    let futures = (0..10000)
+        .map(|batchnum| guarded_batch2sql(&root, batchnum, Arc::clone(&file_limiting_sem)))
+        .collect::<Vec<_>>();
+    use futures::future::join_all;
+    join_all(futures).await.into_iter().collect()
 }
